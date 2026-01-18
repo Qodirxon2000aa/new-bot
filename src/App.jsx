3bd0713e0b6e0d174ad749a36885adec61f4e93a
Qodirxon2000aa <namesuz41@gmail.com>
2026-01-17 16:07:58 -0800

    Sas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
 import Dashboard from "./components/pages/Dashboard";
 import { TelegramProvider } from "../context/TelegramContext";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-
-
-  // 🔥 2-qadam: asosiy ilova
   return (
     <TelegramProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
+        {/* 🔥 Safe area wrapper */}
+        <div className="tg-safe-area">
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
 
-        <SpeedInsights />
+          <SpeedInsights />
+        </div>
       </Router>
     </TelegramProvider>
   );
